78551118b305255e4a5c587dc16738299370cc86
thienhytq-ctrl <thienhytq@gmail.com>
2025-12-10 14:27:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Nguyễn Văn Thiện - Trường  Tiểu học Thành Long - ĐT: 097 2762869</p>
+    <p>Tác giả: Nguyễn Văn Thiện - Trường Tiểu học Thành Long - ĐT: 097 2762869</p>
 </div>
 """, unsafe_allow_html=True)
 
